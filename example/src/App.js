cb5b1adf5d2fdfc846dcdc3f47741cd055f529b8
Michael Edelman <michael@fabulas.io>
2019-09-19 17:19:48 -0400

    feat: add on click/press injection

--- example/src/App.js
+++ example/src/App.js
@@ -3,9 +3,21 @@ import logo from "./logo.svg";
 import "./App.css";
 import { RenderTree } from "@fabulas/astly";
 
-const testTree = `
+const navigate = node => {
+  alert(JSON.stringify(node, null, 2));
+  window.location.href = node.properties.href;
+};
+
+function App() {
+  return <RenderTree tree={getTree()} tools={{ navigate }} />;
+}
+
+export default App;
+
+function getTree() {
+  return `
   <div class="App" >
-    <header minHeight="100vh" style="display: flex; justify-content: center; align-items: center; flex-direction: column;" bg="#282c34" minHeight="100vh" class="App-header">
+    <header style="display: flex; justify-content: center; align-items: center; flex-direction: column;" bg="#282c34" minHeight="100vh" class="App-header">
      <img height="40vmin" src=${logo} source=${logo} class="App-logo" alt="logo"/>
      <p color="white">Edit src/App.js and save to reload.</p>
      <div>
@@ -13,9 +25,4 @@ const testTree = `
       </div>
     </header>
    </div>`;
-
-function App() {
-  return <RenderTree tree={testTree} />;
 }
-
-export default App;

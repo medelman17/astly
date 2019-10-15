b256c6eef652c0e29f9c1f003f7c3a2254e074d5
Michael Edelman <michael@fabulas.io>
2019-10-15 10:11:38 -0400

    Init First Monorepo Package

---
+++ packages/astly/__tests__/astly.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const astly = require('..');
+
+describe('astly', () => {
+    it('needs tests');
+});
---
+++ packages/astly/lib/astly.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = astly;
+
+function astly() {
+    // TODO
+}
